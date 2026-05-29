6db789d783e55674e3c45d7d781947c6359102d4
Javier Dibo <javier.fco.dibo.gomez@gmail.com>
2026-05-29 14:14:21 +0200

    Restructure pipe scene and started spawning them

--- flappy-bird/main.gd
+++ flappy-bird/main.gd
@@ -1,7 +1,24 @@
 extends Node2D
 
+const PipeScene = preload("res://pipe/pipe.tscn")
+var screen_w
+
 func _ready() -> void:
 	Events.touched_something.connect(_on_touched_floor)
+	screen_w = get_viewport_rect().size.x
 
 func _on_touched_floor(message: String) -> void:
 	print(message)
+	spawn_pipe()
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("spawn"):
+		spawn_pipe()
+	
+func spawn_pipe():
+	var pipe = PipeScene.instantiate()
+	var area = pipe.get_node("Area2D/CollisionShape2D")
+	print(area.shape.size)
+	pipe.position = Vector2(screen_w + area.shape.size.y ,500)
+	add_child(pipe)
+	
--- flappy-bird/pipe/pipe.gd
+++ flappy-bird/pipe/pipe.gd
@@ -1,20 +1,15 @@
 extends Node2D
 
-var SPEED = 500
-@onready var width = $Area2D/CollisionShape2D.shape.size.x/2
-		
-func _physics_process(delta: float) -> void:
-	position.x -= SPEED * delta
-	if position.x < 0 - width:
-		position.x = get_viewport_rect().size.x + width
-		pass
-	
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Area2D.body_entered.connect(_on_body_entered)
-	$Area
-	$NinePatchRect.size.y *= 2
+	pass # Replace with function body.
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("bird"):
-		Events.touched_something.emit("Touched a pipe")
-	
+	if (body.is_in_group("bird")):
+		print("Touched the pipe")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	position.x = position.x - 400 * delta
+	pass
